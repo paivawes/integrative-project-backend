413f59756196145c088a7ffff3c899b8b3a8ccfb
paivawes <weslleysilvapaiva@outlook.com>
2024-04-22 21:30:39 -0300

    feat: update routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import { RoomController } from "./presentation/controller/roomController";
+import { scheduleController } from "./presentation/controller/scheduleController";
 import { UserController } from "./presentation/controller/userController";
 
 
@@ -8,5 +10,21 @@ routes.route('/user')
     .get(new UserController().findAll)
     .post(new UserController().create)
 
-    routes.route('/user/id')
+routes.route('/user/id')
     .get(new UserController().findById)
+
+routes.route('/room')
+    .get(new RoomController().create)
+    .post(new RoomController().findAll)
+
+routes.route('/room/id')
+    .get(new RoomController().findById)
+
+    routes.route('/schedule')
+    .get(new scheduleController().create)
+    .post(new scheduleController().findAll)
+
+routes.route('/schedule/id')
+    .get(new scheduleController().findById)
+
+
