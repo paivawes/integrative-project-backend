04b9ea3fabfc713bb718c7b427e06c623cb7ae16
Weslley Paiva <weslleysilvapaiva@outlook.com>
2024-04-23 14:14:36 -0300

    feat: relations between schedule table whit user and room

--- src/presentation/controller/scheduleController.ts
+++ src/presentation/controller/scheduleController.ts
@@ -4,7 +4,7 @@ import { Request, Response } from 'express';
 export class scheduleController {
     async findAll(req: Request, res: Response) {
         try {
-            const schedules = await scheduleRepository.find();
+            const schedules = await scheduleRepository.find({ relations: ["userId", "roomId"] });
 
             return res.status(200).json(schedules)
         } catch (error: any) {
@@ -24,14 +24,15 @@ export class scheduleController {
 
     async create(req: Request, res: Response) {
         try {
-            const { name, userId, roomId, startToScheduling, endToScheduleing } = req.body;
+            const { name, userId, roomId, startToScheduling, endToScheduling } = req.body;
             const schedule = scheduleRepository.create({
                 name,
                 userId,
                 roomId,
                 startToScheduling,
-                endToScheduleing
+                endToScheduling
             })
+            console.log(schedule)
             const userCreate = scheduleRepository.save(schedule)
             return res.status(201).json(userCreate);
         } catch (error: any) {
