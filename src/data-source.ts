57a3008ea73fa785f7c97bcbd415235f25471e95
paivawes <weslleysilvapaiva@outlook.com>
2024-04-30 22:34:42 -0300

    chore: install pg and remove mysql

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,12 +5,13 @@ import { DataSource } from "typeorm";
 const port = process.env.DB_PORT as number | undefined;
 
 export const AppDataSource = new DataSource({
-    type: "mysql",
-    host: process.env.DB_HOST,
-    port: port,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    entities: [`${__dirname}/**/entities/*.{ts,js}`],
-    synchronize: true
-  }); 
+  ssl: { rejectUnauthorized: false },
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: port,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [`${__dirname}/**/entities/*.{ts,js}`],
+  synchronize: true
+}); 
