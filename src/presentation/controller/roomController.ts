4cbf9371bbac3defbe84e4702982aaf45db0dd01
paivawes <weslleysilvapaiva@outlook.com>
2024-05-01 19:05:46 -0300

    feat: room validation

--- src/presentation/controller/roomController.ts
+++ src/presentation/controller/roomController.ts
@@ -15,7 +15,9 @@ export class RoomController {
     async findById(req: Request, res: Response) {
         try {
             const { id } = req.params;
+
             const room = await roomRepository.findOne({ where: { id: id } })
+            
             return res.status(200).json(room)
         } catch (error: any) {
             return res.status(error.status).send(error);
@@ -25,8 +27,17 @@ export class RoomController {
     async create(req: Request, res: Response) {
         try {
             const { name, capacity } = req.body;
-            const user = roomRepository.create({name, capacity})
-            const roomCreate = roomRepository.save(user)
+
+            const existingRoom = await roomRepository.findOne({ where: { name } })
+
+            if (existingRoom) {
+                return res.status(400).json({ error: "O nome da sala fornecido já está em uso." })
+            }
+
+            const user = roomRepository.create({ name, capacity })
+
+            const roomCreate = await roomRepository.save(user)
+
             return res.status(201).json(roomCreate);
         } catch (error: any) {
             return res.status(error.status).send(error);
