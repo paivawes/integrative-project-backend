5732057f3f70f5e57c29a89aac001e0465b06851
Weslley Paiva <weslleysilvapaiva@outlook.com>
2024-05-14 09:50:54 -0300

    feat: test cors origin

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,14 @@
 import express from "express";
 import { AppDataSource } from "./data-source";
 import { routes } from "./routes";
+import cors from "cors"; 
 
-
-var cors = require('cors')
 AppDataSource.initialize().then(() => {
   const app = express();
 
   app.use(express.json());
   
-  app.use(cors({
-    origin: 'http://localhost:5173/'
-  }));
+  app.use(cors());
   app.use(routes);
 
   return app.listen(process.env.PORT);
