7f87400db045a4ab5e07a8af87d5602516ddb16b
paivawes <weslleysilvapaiva@outlook.com>
2024-05-14 21:25:13 -0300

    wip: test 8

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,11 @@ AppDataSource.initialize().then(() => {
   const app = express();
 
   app.use(express.json());
-  
+  app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+  });
   app.use(cors({origin: '*'}));
   app.use(routes);
 
