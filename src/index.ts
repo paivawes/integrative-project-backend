ec2da3834c344bcb49c668e7c4a03f8ca1bd0503
Weslley Paiva <weslleysilvapaiva@outlook.com>
2024-05-15 14:04:32 -0300

    wip: teste

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import express from "express";
 import { AppDataSource } from "./data-source";
 import { routes } from "./routes";
 import cors from "cors"; 
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 AppDataSource.initialize().then(() => {
   const app = express();
-
+  app.use('/api', createProxyMiddleware({ target: 'https://integrative-project-backend.vercel.app', changeOrigin: true }));
+  app.use(express.static('public'));
   app.use(express.json());
   app.use(cors());
   app.use(routes);
