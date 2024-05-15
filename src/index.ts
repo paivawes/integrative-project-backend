edf8c1cc2aac9f730941527b03be19fb2538245d
Weslley Paiva <weslleysilvapaiva@outlook.com>
2024-05-15 14:06:52 -0300

    remove cors

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
 import express from "express";
 import { AppDataSource } from "./data-source";
 import { routes } from "./routes";
-import cors from "cors"; 
-import { createProxyMiddleware } from "http-proxy-middleware";
 
 AppDataSource.initialize().then(() => {
   const app = express();
-  app.use('/api', createProxyMiddleware({ target: 'https://integrative-project-backend.vercel.app', changeOrigin: true }));
-  app.use(express.static('public'));
   app.use(express.json());
-  app.use(cors());
+  // app.use(cors());
   app.use(routes);
 
   return app.listen(process.env.PORT);
