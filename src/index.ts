e2bb75547e3e2d94509307ca4c0a6f815c1bf991
Weslley Paiva <weslleysilvapaiva@outlook.com>
2024-05-16 13:40:03 -0300

    teste

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import { AppDataSource } from "./data-source";
 import { routes } from "./routes";
+import cors from "cors";
 
 AppDataSource.initialize().then(() => {
   const app = express();
   app.use(express.json());
-  // app.use(cors());
+  app.use(cors({
+    origin: 'http://localhost:5173'
+  }));
   app.use(routes);
 
   return app.listen(process.env.PORT);
